55cc3e2a5c4d78b13d2e8430d9792f87150824ad
Mahendra Kumawat <117855423+mk016@users.noreply.github.com>
2024-03-18 20:38:24 +0000

    backend is complete and frontend ready coming soon

---
+++ forntend/basic project/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+  return (
+  
+    function App() {
+      const [jokes, setJokes] = useState([])
+      return (
+        <>
+        <h1>SYSTEM HANG HO GYA H</h1>
+        <h2> jokes: {jokes.length}</h2>
+        </>
+      )
+    }
+    
+  )
+
+export default App
